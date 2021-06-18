811c9d3bd4f08c8f6519c43289cc22d5b1414624
Andres <37418332+kustom89@users.noreply.github.com>
2021-06-18 16:16:45 -0400

    heroku json

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("/api/destinatarios", require("./routes/destinatario.routes"));
 //   console.log("servidor corriendo en el puerto:  ", process.env.port || 3000);
 // });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 3000;
 app.listen(PORT, () => {
   console.log(`servidor corriendo en el puerto:  ${PORT}`);
 });
