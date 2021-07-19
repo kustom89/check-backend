9926a1cd0835b0650093f3bfdfbabf643de8b309
Andres <37418332+kustom89@users.noreply.github.com>
2021-07-19 17:49:29 -0400

    eliminacion de logs

--- public/main.js
+++ public/main.js
@@ -276,7 +276,7 @@ if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].produc
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["enableProdMode"])();
 }
 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["platformBrowser"]().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
-    .catch(err => console.error(err));
+    .catch(err => err);
 
 
 /***/ }),
